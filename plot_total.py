a85a20a814c615e2acad81aa512d6a9147e9317e
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-12-02 21:24:58 -0300

    update graph

--- plot_total.py
+++ plot_total.py
@@ -19,13 +19,15 @@ def plot_execution_times(times_cpu,
                          python_impl,
                          omp,
                          cuda_png,
+                         cuda_dicm,
                          name):
     cpu = pd.DataFrame({"fn": ["355 DICOM CPU"], "time": [times_cpu[0]]})
     gpu_omp = pd.DataFrame({"fn": ["355 DICOM CUDA+OMP"], "time": [times_gpu[0]]})
     omp= pd.DataFrame({"fn": ["355-Images-PNG with OMP+CUDA"], "time": [omp[0]]})
     python = pd.DataFrame({"fn": ["355 DICOM python Cuda"], "time": [python_impl[0]]})
     cuda_png= pd.DataFrame({"fn": ["355 Images-PNG Cuda"], "time": [cuda_png[0]]})
-    data = pd.concat([cpu, gpu_omp,cuda_png , omp, python]).reset_index(drop=True)
+    cuda_dicm= pd.DataFrame({"fn": ["355 DICOM/V Images Cuda"], "time": [cuda_dicm[0]]})
+    data = pd.concat([cpu, gpu_omp,cuda_png , omp, cuda_dicm, python]).reset_index(drop=True)
     plt.figure(figsize=(12, 7))
     plt.bar(data["fn"], data["time"], color=["skyblue", "salmon"])
     plt.xlabel('Process')
@@ -42,4 +44,6 @@ if __name__ == "__main__":
     _, python_impl= read_csv("./benchmark_times.csv")
     _, omp_cuda_png= read_csv("./total_gpu_png_omp.csv")
     _, cuda_png= read_csv("./total_gpu_png.csv")
-    plot_execution_times(times_cpu, times_gpu, python_impl, omp_cuda_png, cuda_png, "total_time_comparison.png")
+    _, cuda_dicm= read_csv("./total_gpu_omp_cuda_only.csv")
+    plot_execution_times(times_cpu, times_gpu, python_impl, 
+                         omp_cuda_png, cuda_png, cuda_dicm, "total_time_comparison.png")
